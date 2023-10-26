c25cd4e80900568e66cb4217364d3044b4f6ee0d
BRUNOOCRUZZ <brunoocruzz01@gmail.com>
2023-10-25 23:33:44 -0300

    pequeno exercicio pratico

---
+++ index.js
@@ -0,0 +1,23 @@
+let name = "Bruno"
+
+let experience = 5000
+
+if (experience <= 1000) {
+  console.log(`O Herói de nome ${name} está no nível de ${experience}`)
+}else if ( experience >= 1001 && experience <= 2000 ) {
+  console.log(`O Herói de nome ${name} está no nível de ${experience}`)
+}else if (experience >= 2001 && experience <= 5000) {
+  console.log(`O Herói de nome ${name} está no nível de ${experience}`)  
+}else if (experience >= 5001 && experience <= 6000) {
+  console.log(`O Herói de nome ${name} está no nível de ${experience}`)  
+}else if (experience >= 6001 && experience <= 7000) {
+  console.log(`O Herói de nome ${name} está no nível de ${experience}`)  
+}else if (experience >= 7001 && experience <= 8000) {
+  console.log(`O Herói de nome ${name} está no nível de ${experience}`)  
+}else if (experience >= 8001 && experience <= 9000) {
+  console.log(`O Herói de nome ${name} está no nível de ${experience}`)  
+}else if (experience >= 9001 && experience <= 10000) {
+  console.log(`O Herói de nome ${name} está no nível de ${experience}`)  
+}else if (experience >= 10001) {
+  console.log(`O Herói de nome ${name} está no nível de ${experience}`)  
+}
